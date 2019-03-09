af0b125dcc76aa66567e410f8b8a8be2a00c54c4
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-09 09:44:06 -0500

    Update raspbian.sh

--- installers/raspbian.sh
+++ installers/raspbian.sh
@@ -1,6 +1,7 @@
 UPDATE_URL="https://raw.githubusercontent.com/necro-nemesis/raspap-webgui/master/"
 wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/raspapcommon.sh
-source /tmp/raspapcommon.sh && rm -f /tmp/raspapcommon.sh
+source /tmp/raspapcommon.sh
+#&& rm -f /tmp/raspapcommon.sh
 
 function update_system_packages() {
     install_log "Updating sources"
