5a6afe40011aa91e64dbbd73f4761be89004c10a
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-16 22:43:50 -0400

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -495,10 +495,8 @@ function SaveTORAndVPNConfig(){
     foreach( $return as $line ) {
       echo htmlspecialchars($line, ENT_QUOTES).'<br />' , PHP_EOL;
     }
-  }
+  }	
 
-<?php
-{	
 /**
 *
 *
@@ -517,6 +515,128 @@ function DisplayLokinetConfig(){
 		$status = '<div class="alert alert-success alert-dismissable">Lokinet is running
 					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>';
 	}
-    }
+	$arrConfig = array();
+	foreach( $return as $a ) {
+		if( $a[0] != "#" ) {
+			$arrLine = explode( " ",$a) ;
+			$arrConfig[$arrLine[0]]=$arrLine[1];
+		}
+	}
+
+?>
+	<div class="row">
+	<div class="col-lg-12">
+		<div class="panel panel-primary">
+			<div class="panel-heading"><i class="fa fa-eye-slash fa-fw"></i> Configure TOR proxy</div>
+        <!-- /.panel-heading -->
+        <div class="panel-body">
+        	<!-- Nav tabs -->
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#basic" data-toggle="tab">Basic</a>
+                </li>
+                <li><a href="#relay" data-toggle="tab">Relay</a>
+                </li>
+            </ul>
+
+            <!-- Tab panes -->
+           	<div class="tab-content">
+           		<p><?php echo $status; ?></p>
+
+            	<div class="tab-pane fade in active" id="basic">
+            		<h4>Basic settings</h4>
+					<form role="form" action="?page=save_hostapd_conf" method="POST">
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">VirtualAddrNetwork</label>
+							<input type="text" class="form-control" name="virtualaddrnetwork" value="<?php echo htmlspecialchars($arrConfig['VirtualAddrNetwork'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">AutomapHostsSuffixes</label>
+							<input type="text" class="form-control" name="automaphostssuffixes" value="<?php echo htmlspecialchars($arrConfig['AutomapHostsSuffixes'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">AutomapHostsOnResolve</label>
+							<input type="text" class="form-control" name="automaphostsonresolve" value="<?php echo htmlspecialchars($arrConfig['AutomapHostsOnResolve'], ENT_QUOTES); ?>" />
+						</div>
+					</div>	
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">TransListenAddress</label>
+							<input type="text" class="form-control" name="translistenaddress" value="<?php echo htmlspecialchars($arrConfig['TransListenAddress'], ENT_QUOTES); ?>" />
+						</div>
+					</div>	
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">DNSPort</label>
+							<input type="text" class="form-control" name="dnsport" value="<?php echo htmlspecialchars($arrConfig['DNSPort'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">DNSListenAddress</label>
+							<input type="text" class="form-control" name="dnslistenaddress" value="<?php echo htmlspecialchars($arrConfig['DNSListenAddress'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane fade" id="relay">
+            		<h4>Relay settings</h4>
+            		<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">ORPort</label>
+							<input type="text" class="form-control" name="orport" value="<?php echo htmlspecialchars($arrConfig['ORPort'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">ORListenAddress</label>
+							<input type="text" class="form-control" name="orlistenaddress" value="<?php echo htmlspecialchars($arrConfig['ORListenAddress'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">Nickname</label>
+							<input type="text" class="form-control" name="nickname" value="<?php echo htmlspecialchars($arrConfig['Nickname'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">Address</label>
+							<input type="text" class="form-control" name="address" value="<?php echo htmlspecialchars($arrConfig['Address'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">RelayBandwidthRate</label>
+							<input type="text" class="form-control" name="relaybandwidthrate" value="<?php echo htmlspecialchars($arrConfig['RelayBandwidthRate'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+					<div class="row">
+						<div class="form-group col-md-4">
+							<label for="code">RelayBandwidthBurst</label>
+							<input type="text" class="form-control" name="relaybandwidthburst" value="<?php echo htmlspecialchars($arrConfig['RelayBandwidthBurst'], ENT_QUOTES); ?>" />
+						</div>
+					</div>
+				</div>
+
+				<input type="submit" class="btn btn-outline btn-primary" name="SaveTORProxySettings" value="Save settings" />
+				<?php 
+				if( $torproxystatus[0] == 0 ) {
+					echo '<input type="submit" class="btn btn-success" name="StartTOR" value="Start TOR" />' , PHP_EOL;
+				} else {
+					echo '<input type="submit" class="btn btn-warning" name="StopTOR" value="Stop TOR" />' , PHP_EOL;
+				};
+				?>
+				</form>
+			</div><!-- /.tab-content -->
+		</div><!-- /.panel-body -->
+		<div class="panel-footer"> Information provided by tor</div>
+    </div><!-- /.panel-primary -->
+</div><!-- /.col-lg-12 -->
+</div><!-- /.row -->
+<?php 
 }
 }
