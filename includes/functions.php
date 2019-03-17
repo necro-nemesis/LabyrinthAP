4450beef7beb6bcdd1aec1af9f04316d2b5acedc
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-17 09:49:59 -0400

    Remove container

--- includes/functions.php
+++ includes/functions.php
@@ -433,9 +433,9 @@ function DisplayLokinetConfig(){
           <div class="tab-pane fade" id="youtube">
             <div class="row">
               <div class="col-lg-12">
-                <div class="container">
                 <h2>This is Loki</h2>
-                      <iframe src="https://www.youtube.com/embed/Lykh-NqkKys"></iframe>
+                <div class="embed-responsive embed-responsive-16by9">
+                      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Lykh-NqkKys"></iframe>
                     </div>
                   </div>
                 </div>
