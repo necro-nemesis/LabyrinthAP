3414b989bb82298cf4b614274f9b7e514f27213d
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-17 09:40:44 -0400

    row col first

--- includes/functions.php
+++ includes/functions.php
@@ -431,10 +431,10 @@ function DisplayLokinetConfig(){
             </div><!-- /.row -->
 
           <div class="tab-pane fade" id="youtube">
-              <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="container">
                 <h2>This is Loki</h2>
-                <div class="row">
-                  <div class="col-lg-12">
                     <div class="embed-responsive embed-responsive-16by9">
                       <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Lykh-NqkKys"></iframe>
                     </div>
