e9c98895a1c450dbe332aede8c1d19f32f1a0fe5
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-17 15:13:50 -0400

    shorten wording

--- includes/functions.php
+++ includes/functions.php
@@ -432,10 +432,11 @@ function DisplayLokinetConfig(){
                 echo '<input type="submit" class="btn btn-success" name="StartLokinet" value="Start Lokinet" />' , PHP_EOL;
               } elseif {
                 echo '<input type="submit" class="btn btn-warning" name="StopLokinet" value="Stop Lokinet" />' , PHP_EOL;
-              } elsif( 1=1 ) {
-                echo '<input type="submit" class="btn btn-success" name="GenerateLokinet" value="Generate Lokinet" />' , PHP_EOL;
+              }
+              if( 1=1 ) {
+                echo '<input type="submit" class="btn btn-success" name="GenerateLokinet" value="Generate" />' , PHP_EOL;
               } else {
-                echo '<input type="submit" class="btn btn-warning" name="RegenLokinet" value="Re-Generate Lokinet" />' , PHP_EOL;
+                echo '<input type="submit" class="btn btn-warning" name="RegenLokinet" value="Re-Generate" />' , PHP_EOL;
               };
               ?>
 
