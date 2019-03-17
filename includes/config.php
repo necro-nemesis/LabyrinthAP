3e635f6cb551118966046dcc17d99689fcc39523
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-17 18:42:09 -0400

    Deactivate Tor

--- includes/config.php
+++ includes/config.php
@@ -26,7 +26,7 @@ define('RASPI_HOTSPOT_ENABLED', true );
 define('RASPI_NETWORK_ENABLED', true );
 define('RASPI_DHCP_ENABLED', true );
 define('RASPI_OPENVPN_ENABLED', false );
-define('RASPI_TORPROXY_ENABLED', true );
+define('RASPI_TORPROXY_ENABLED', false );
 define('RASPI_CONFAUTH_ENABLED', true );
 define('RASPI_CHANGETHEME_ENABLED', true );
 define('RASPI_VNSTAT_ENABLED', true );
