6549378d6c4f72bd4d1543275b2d436b29248853
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-17 09:54:05 -0400

    Swap col for container

--- includes/functions.php
+++ includes/functions.php
@@ -431,16 +431,13 @@ function DisplayLokinetConfig(){
             </div><!-- /.row -->
 
           <div class="tab-pane fade" id="youtube">
-            <div class="row">
-              <div class="col-lg-12">
+            <div class="container">
                 <h2>This is Loki</h2>
                 <div class="embed-responsive embed-responsive-16by9">
                       <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Lykh-NqkKys"></iframe>
-                    </div>
-                  </div>
                 </div>
-              </div>
             </div>
+          </div>
 <?php
 }
 
