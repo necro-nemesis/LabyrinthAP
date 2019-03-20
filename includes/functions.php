6416568ed6f33216eb921fea2f20842227c5d126
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-20 01:26:36 -0400

    cooma

--- includes/functions.php
+++ includes/functions.php
@@ -603,7 +603,7 @@ function SaveTORAndVPNConfig(){
 //    header("Refresh:0");
   } elseif( isset($_POST['StopLokinet']) ) {
     echo "Attempting to stop Lokinet";
-    exec( 'kill -9 lokinet );
+    exec( 'kill -9 lokinet' );
 //    location.reload();
   } elseif( isset($_POST['GenerateLokinet']) ) {
     echo "Attempting to generate Lokinet";
