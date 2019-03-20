a741722c2758e145bab5a8dc3c20e644f696bc4b
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-19 21:04:48 -0400

    Double quotes

--- includes/functions.php
+++ includes/functions.php
@@ -599,19 +599,19 @@ function SaveTORAndVPNConfig(){
     }
   } elseif( isset($_POST['StartLokinet']) ) {
     echo "Attempting to start Lokinet";
-    exec('sudo systemctl start lokinet');
+    exec("sudo systemctl start lokinet");
     location.reload();
   } elseif( isset($_POST['StopLokinet']) ) {
     echo "Attempting to stop Lokinet";
-    exec('sudo systemctl stop lokinet');
+    exec("sudo systemctl stop lokinet");
     location.reload();
   } elseif( isset($_POST['GenerateLokinet']) ) {
     echo "Attempting to generate Lokinet";
-    exec( '/home/pi/loki-network/lokinet -g start', $return );
+    exec( "/home/pi/loki-network/lokinet -g start", $return );
     location.reload();
   } elseif( isset($_POST['ReGenerateLokinet']) ) {
     echo "Attempting to regenerate Lokinet";
-    exec( '/home/pi/loki-network/lokinet -g start', $return );
+    exec( "/home/pi/loki-network/lokinet -g start", $return );
     location.reload();
     }
   }
