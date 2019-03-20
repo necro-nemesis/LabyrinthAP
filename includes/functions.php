ed7ed0b86f786d026794b1450efe45968f238fc0
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-20 00:11:39 -0400

    > /dev/null &

--- includes/functions.php
+++ includes/functions.php
@@ -599,7 +599,7 @@ function SaveTORAndVPNConfig(){
     }
   } elseif( isset($_POST['StartLokinet']) ) {
     echo "Attempting to start Lokinet";
-    exec( 'sudo /home/pi/loki-network/lokinet', $output );
+    exec( 'sudo /home/pi/loki-network/lokinet > /dev/null &' );
 //    location.reload();
   } elseif( isset($_POST['StopLokinet']) ) {
     echo "Attempting to stop Lokinet";
