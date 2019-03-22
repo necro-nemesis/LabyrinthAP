bab28ca5839766166aa461ab81972df030596e81
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-21 21:44:39 -0400

    remove quotes in ascii

--- installers/common.sh
+++ installers/common.sh
@@ -47,12 +47,12 @@ function display_welcome() {
 
     echo -e "${green}\n"
     echo -e "  ooooo                  oooo         o8o        .o.       ooooooooo."
-    echo -e "  `888'                  `888         `"'       .888.      `888   `Y88."
-    echo -e "   888          .ooooo.   888  oooo  oooo      .8"888.      888   .d88'"
-    echo -e "   888         d88' `88b  888 .8P'   `888     .8' `888.     888ooo88P'"
+    echo -e "   888                    888                   .888.       888    Y88."
+    echo -e "   888          .ooooo.   888  oooo  oooo      .8 888.      888   .d88"
+    echo -e "   888         d88   88b  888 .8P     888     .8   888.     888ooo88P"
     echo -e "   888         888   888  888888.     888    .88ooo8888.    888"
-    echo -e "   888       o 888   888  888 `88b.   888   .8'     `888.   888"
-    echo -e " o888ooooood8 `Y8bod8P' o888o o888o o888o o88o     o8888o o888o"
+    echo -e "   888       o 888   888  888  88b.   888   .8       888.   888"
+    echo -e " o888ooooood8   Y8bod8P o888o o888o o888o o88o     o8888o o888o"
     echo -e "${raspberry}"
     echo -e "The Quick Installer will guide you through a few easy steps\n\n"
 }
