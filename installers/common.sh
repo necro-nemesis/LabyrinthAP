f3de639626fa34dc474cd764317000da6cde33e9
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-21 23:54:21 -0400

    chonw and chmod lokilaunch.sh

--- installers/common.sh
+++ installers/common.sh
@@ -197,6 +197,8 @@ function default_configuration() {
     sudo mv $webroot_dir/config/dnsmasq.conf /etc/dnsmasq.conf || install_error "Unable to move dnsmasq configuration file"
     sudo mv $webroot_dir/config/dhcpcd.conf /etc/dhcpcd.conf || install_error "Unable to move dhcpcd configuration file"
     sudo mv $webroot_dir/config/lokilaunch.sh /home/pi/loki-network/lokilaunch.sh || install error "Unable to move, install Lokinet first"
+    sudo chmod 755 /home/pi/loki-network/lokilaunch.sh 
+    sudo chown pi:pi lokilaunch.sh
 
     # Generate required lines for Rasp AP to place into rc.local file.
     # #RASPAP is for removal script
