d90a88a51f25bf98693ff2a87a33b92d1c704837
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-31 21:00:11 -0400

    fix encapsd

--- includes/functions.php
+++ includes/functions.php
@@ -634,7 +634,7 @@ function SaveTORAndVPNConfig(){
     </div>
     <?php
   //echo "lokinet-bootstrap ", $bootstrap;
-  $output = shell_exec('sudo /home/pi/loki-network/lokilaunch.sh "bootstrap", $bootstrap);
+  $output = shell_exec( 'sudo /home/pi/loki-network/lokilaunch.sh bootstrap', $bootstrap );
   echo "<pre>$output</pre>";
 
 }
