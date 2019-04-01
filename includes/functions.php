cbc8878980d502c838fc513489bcc48ed84258e7
necro-nemesis <necro_nemesis@hotmail.com>
2019-03-31 21:45:30 -0400

    more passing argument to shell changes

--- includes/functions.php
+++ includes/functions.php
@@ -634,8 +634,8 @@ function SaveTORAndVPNConfig(){
     </div>
     <?php
   //echo "lokinet-bootstrap ", $bootstrap;
-  $escapedArgumment = escapeshellarg($bootstrap);
-  $output = shell_exec( "sudo /home/pi/loki-network/lokilaunch.sh bootstrap $bootstrap" );
+  $bootstrap = escapeshellarg($bootstrap);
+  $output = shell_exec("sudo /home/pi/loki-network/lokilaunch.sh bootstrap {$bootstrap}");
   echo "<pre>$output</pre>";
 
 }
