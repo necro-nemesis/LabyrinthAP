60afac164c347e4482aa8f7f7c7b4d5c3ba9ba79
necro-nemesis <necro_nemesis@hotmail.com>
2019-04-03 18:22:29 -0400

    Bash script to stop daemon before applying bootstrap

--- config/lokilaunch.sh
+++ config/lokilaunch.sh
@@ -17,10 +17,15 @@ case "$1" in
         ;;
 
 bootstrap)
+        echo -n "LOKINET DAEMON SHUTDOWN FOR BOOTSTRAPPING\n"
+        pkill lokinet
+        sleep 2
+        pidof lokinet >/dev/null && echo "Service is running\n" || echo "Service NOT running\n"
         echo -n "FETCH BOOTSTRAP <---- "
         /home/pi/loki-network/lokinet-bootstrap "$2"
-        echo -n "SUCCESS! BOOTSTRAPPED WTIH ---> $2"
+        echo -n "SUCCESS! BOOTSTRAPPED WTIH ---> $2\n\n"
         cp /home/pi/loki-network/lokinet-bootstrap.exe /root/.lokinet/
+        echo -n "MANUALLY RESTART LOKINET DAEMON TO RECONNECT TO SERVICE\n"
         ;;
   *)
         echo "Usage: "$1" {start|stop|gen|bootstrap}"
