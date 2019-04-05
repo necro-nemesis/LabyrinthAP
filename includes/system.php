288ad55ee79176b6b009b2d93a88338b8c8b0151
necro-nemesis <necro_nemesis@hotmail.com>
2019-04-05 07:44:08 -0400

    correct terminology

--- includes/system.php
+++ includes/system.php
@@ -181,8 +181,8 @@ function DisplaySystem(){
           <input type="submit" class="btn btn-warning" name="system_shutdown" value="<?php echo _("Shutdown"); ?>" />
           <input type="button" class="btn btn-outline btn-primary" value="<?php echo _("Refresh"); ?>" onclick="document.location.reload(true)" />
           </form>
-          <h5><p class="font-weight-bold"><?php echo _("Your development support is greatly appreciated: Developer Loki Address"); ?></bold></h5>
-          <h5><p class="font-weight-bold"><pre><?php echo _("LK8CGQ17G9R3ys3Xf33wCeViD2B95jgdpjAhcRsjuheJ784dumXn7g3RPAzedWpFq364jJKYL9dkQ8mY66sZG9BiCwrYHPmcZin1VP8Btf"); ?></pre></bold></h5>
+          <h5><strong><?php echo _("Your development support is greatly appreciated: Developer Loki Address"); ?></strong></h5>
+          <h5><strong><pre><?php echo _("LK8CGQ17G9R3ys3Xf33wCeViD2B95jgdpjAhcRsjuheJ784dumXn7g3RPAzedWpFq364jJKYL9dkQ8mY66sZG9BiCwrYHPmcZin1VP8Btf"); ?></pre></strong></h5>
 
         </div>
       </div>
