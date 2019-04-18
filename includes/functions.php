89ec7a018d60cbd255a69eb3fd7e8cdaa1a84d3d
necro-nemesis <necro_nemesis@hotmail.com>
2019-04-17 22:27:30 -0400

    delay 5 seconds to restart dnsmasq

--- includes/functions.php
+++ includes/functions.php
@@ -604,6 +604,7 @@ function SaveTORAndVPNConfig()
     <?php
     shell_exec('/etc/init.d/dnsmasq stop > /dev/null &');
     $output = shell_exec('sudo /home/pi/loki-network/lokilaunch.sh start');
+    sleep(5);
     shell_exec('/etc/init.d/dnsmasq start > /dev/null &');
     echo "<pre><strong>$output</strong></pre>";
     } elseif (isset($_POST['StopLokinet'])) {
