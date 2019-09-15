f279c1c3d86aa123219a9c2071f1fff73fa0e46b
necro-nemesis <necro_nemesis@hotmail.com>
2019-09-15 16:02:11 -0400

    All lokinet stop/stop scripts use systemctl

--- config/lokilaunch.sh
+++ config/lokilaunch.sh
@@ -28,14 +28,13 @@ case "$1" in
         ;;
 
 bootstrap)
-        echo -n "LOKINET DICONNECTED AND DAEMON SHUTDOWN FOR BOOTSTRAPPING\n"
-        pkill lokinet
-        sleep 2
-        pidof lokinet >/dev/null && echo "Service is running\n" || echo "Service NOT running\n"
-        echo -n "FETCH BOOTSTRAP <---- "
+        echo -n "Lokinet daemon shutdown for bootstrapping\n"
+        systemctl stop lokinet
+        pidof lokinet >/dev/null && echo "Daemon shutdown failure\n" || echo "Daemon is stopped\n"
+        echo -n "Fetching bootstrap <---- "
         lokinet-bootstrap "$2"
-        echo -n "SUCCESS! BOOTSTRAPPED WTIH ---> $2\n\n"
-        echo -n "YOU MUST MANUALLY RESTART LOKINET DAEMON AND RECONNECT FOR SERVICE\n"
+        echo -n "Bootstrapped with ---> $2\n\n"
+        systemclt start lokinet
         ;;
 
   *)
