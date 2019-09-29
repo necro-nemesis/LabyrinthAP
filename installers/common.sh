ec6c334b676080d6cef13982713659581cf86fc4
necro-nemesis <necro_nemesis@hotmail.com>
2019-09-29 00:05:39 -0400

    comment out crontab

--- installers/common.sh
+++ installers/common.sh
@@ -338,13 +338,13 @@ function patch_system_files() {
     sudo systemctl enable hostapd.service
 
     #crontab daily lokinet updates and log
-    cat > /var/spool/cron/crontabs/root <<- "EOF"
+    #cat > /var/spool/cron/crontabs/root <<- "EOF"
     #check daily for lokinet updates and update as required
-    logfile=/var/log/lokinet_cron_update.txt
-    0 1 * * 1-7 sudo apt-get update && sudo apt-get -y install lokinet >> "$logfile" 2>&1
-    0 1 * * 1-7 sudo apt-get -y autoremove >> "$logfile" 2>&1
-    0 1 * * 1-7 date >> "$logfile"
-    EOF
+    #logfile=/var/log/lokinet_cron_update.txt
+    #0 1 * * 1-7 sudo apt-get update && sudo apt-get -y install lokinet >> "$logfile" 2>&1
+    #0 1 * * 1-7 sudo apt-get -y autoremove >> "$logfile" 2>&1
+    #0 1 * * 1-7 date >> "$logfile"
+    #EOF
     }
 
 
