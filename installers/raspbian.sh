6b9f0ddcf43b8804190bc21385f8111c403d4ec1
necro-nemesis <necro_nemesis@hotmail.com>
2020-03-17 16:07:30 -0400

    revert back to lokinet release binary.

--- installers/raspbian.sh
+++ installers/raspbian.sh
@@ -12,12 +12,12 @@ function install_dependencies() {
     sudo apt-get -y install curl
     echo "Install public key used to sign the lokinet binaries."
     curl -s https://deb.imaginary.stream/public.gpg | sudo apt-key add -
-    echo "deb https://deb.imaginary.stream/beta $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/imaginary.stream.list
+    echo "deb https://deb.imaginary.stream $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/imaginary.stream.list
     sudo apt-get update
     sudo yes | apt-get install lighttpd $php_package git resolvconf hostapd dnsmasq vnstat lokinet || install_error "Unable to install dependencies"
 }
 
-#Remove NetworkManager and install dhcpd if required.
+#Remove NetworkManager and install dhcpd if required Armbian.
 
 function check_for_networkmananger() {
   install_log "Checking for NetworkManager"
